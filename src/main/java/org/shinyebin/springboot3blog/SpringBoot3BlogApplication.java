7e422611d77572b5f945ccc5b466e7799801a3da
sinyebin <dpqls0625@naver.com>
2023-11-19 16:37:25 +0900

    init

---
+++ src/main/java/org/shinyebin/springboot3blog/SpringBoot3BlogApplication.java
@@ -0,0 +1,11 @@
+package org.shinyebin.springboot3blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3BlogApplication.class,args);
+    }
+}
